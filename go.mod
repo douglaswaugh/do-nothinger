ee26157230fadee9827e9890676155a4d63444df
douglas waugh <douglas.waugh.e3@gmail.coms>
2025-11-27 16:30:36 +0200

    feat: hello, Do Nothinger initial setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/douglaswaugh/do-nothinger
+
+go 1.25.4
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestPlaceholder(t *testing.T) {
+	// Placeholder test to verify test framework works
+	// This will be replaced with real tests during TDD
+	if false {
+		t.Error("This should never fail")
+	}
+}

f75642458963983a2fb64da0a685a29fdb72f4c1
douglas waugh <douglas.waugh.e3@gmail.coms>
2025-12-03 17:41:47 +0200

    refactor: split main() and run() tests in to separate files

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"os"
 	"os/exec"
 	"strings"
@@ -26,23 +25,5 @@ func TestMain_CallsRunWithScriptPath(t *testing.T) {
 
 	if !strings.Contains(string(output), "Done") {
 		t.Errorf("Expected output to contain 'Done', got: %s", output)
-	}
-}
-
-func TestRunScriptWithZeroSteps_DisplaysDone(t *testing.T) {
-	scriptFile, err := os.CreateTemp("", "script-*.sh")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(scriptFile.Name())
-
-	scriptFile.WriteString("#!/bin/bash\n")
-	scriptFile.Close()
-
-	var output bytes.Buffer
-	run(scriptFile.Name(), &output)
-
-	if !strings.Contains(output.String(), "Done") {
-		t.Errorf("Expected output to contain 'Done', got: %s", output.String())
 	}
 }
---
+++ run_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestRunScriptWithZeroSteps_DisplaysDone(t *testing.T) {
+	scriptFile, err := os.CreateTemp("", "script-*.sh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(scriptFile.Name())
+
+	scriptFile.WriteString("#!/bin/bash\n")
+	scriptFile.Close()
+
+	var output bytes.Buffer
+	run(scriptFile.Name(), &output)
+
+	if !strings.Contains(output.String(), "Done") {
+		t.Errorf("Expected output to contain 'Done', got: %s", output.String())
+	}
+}
